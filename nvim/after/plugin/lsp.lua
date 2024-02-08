3ddcd8b9e128809fc7ceaf664036a26b7100602b
Jan <janborovsky01@gmail.com>
2024-02-08 16:35:39 +0100

    Auto commit.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -44,6 +44,7 @@ local on_attach = function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  -- vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts) -- can't be this, it's just not working
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
