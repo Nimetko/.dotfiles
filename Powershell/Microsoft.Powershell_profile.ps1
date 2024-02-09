3dcfe8db56e592741b8db364669a6cf1858e7eae
Jan <janborovsky01@gmail.com>
2024-02-09 14:15:36 +0100

    Add Powershell config sync

---
+++ Powershell/Microsoft.Powershell_profile.ps1
@@ -0,0 +1,9 @@
+Set-Alias -Name update -Value '~\x\scripts\windows\UpdateRepositories.ps1'
+Set-Alias -Name push -Value '~\x\scripts\windows\AutoPushRepos.ps1'
+Set-Alias -Name status -Value '~\x\scripts\windows\StatusRepositories.ps1'
+Set-Alias -Name lf -Value '~\x\scripts\windows\FuzzyFinderInDirectory.ps1'
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name v -Value nvim
+
+Set-Alias -Name cl -Value clear
+
