df65ad44b833623bc2ee97624808146ce08efca2
Nimetko <janborovsky01@gmail.com>
2024-03-03 04:56:08 +0100

    Auto commit.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -14,7 +14,7 @@ autocmd("BufWinEnter", {
 
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "<leader>p", function()
+        vim.keymap.set("n", "<leader>P", function()
             vim.cmd.Git('push')
         end, opts)
 
@@ -23,7 +23,7 @@ autocmd("BufWinEnter", {
         end, opts)
 
         -- rebase always
-        vim.keymap.set("n", "<leader>P", function()
+        vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git({'pull',  '--rebase'})
         end, opts)
 
