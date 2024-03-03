553f4574d6f2f2ce9fe0e262b748e7a7b81d07d8
Nimetko <janborovsky01@gmail.com>
2024-03-03 04:50:33 +0100

    lsp for dart and c#. working on mac.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,20 +1,11 @@
-local lsp_zero = require("lsp-zero")
 local lsp_config = require("lspconfig")
+local lsp = require("lsp-zero")
 
-lsp_zero.preset("recommended")
-
-lsp_zero.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'lua_ls',
-})
-
--- Fix Undefined global 'vim'
-lsp_zero.nvim_workspace()
+lsp.preset("recommended")
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
@@ -24,11 +15,11 @@ local cmp_mappings = lsp_zero.defaults.cmp_mappings({
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
-lsp_zero.setup_nvim_cmp({
+lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
-lsp_zero.set_preferences({
+lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
         error = 'E',
@@ -38,25 +29,53 @@ lsp_zero.set_preferences({
     }
 })
 
-local on_attach = function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+local function myFunction(opts)
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts) vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts) -- vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts) -- can't be this, it's just not working
+vim.keymap.set("n", "<leader>lsp", function()
+    print("LSP")
+end)
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts) -- vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts) -- can't be this, it's just not working
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+
+  vim.keymap.set("n", "gr", function()
+    require('telescope.builtin').lsp_references({use_quickfix = true})
+end)
+
+  vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
   vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.formatting() end, opts)
+
 end
 
-lsp_zero.on_attached = on_attach
+-- Define your on_attach function
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true, buffer=bufnr }
+
+  vim.api.nvim_buf_del_keymap(bufnr, 'n', 'gr')
+  myFunction(opts);
+end
+
+local on_attach_dart = function(client, bufnr)
+  local opts = { noremap=true, silent=true, buffer=bufnr }
+
+  myFunction(opts);
+end
+
+
+-- Set on_attach function for all LSP servers managed by lsp-zero
+lsp.on_attach(on_attach)
+
+-- lsp.on_attached = on_attach
 
 lsp_config["dartls"].setup({
-  on_attach = on_attach,
+  -- on_attach = on_attach,
+  on_attach = on_attach_dart,
   settings = {
     dart = {
       analysisExcludedFolders = {
@@ -69,48 +88,28 @@ lsp_config["dartls"].setup({
   },
 })
 
-
-
-
-
-local utils = require('nimet/utils')
-local is_windows = utils.isWindows()
-
--- Assuming lsp_config and on_attach are defined elsewhere in your script
-lsp_config["omnisharp"].setup{
-  on_attach = on_attach
-}
-
-if is_windows then
-  lsp_config["omnisharp"].cmd = { "C:\\Users\\janbo\\.omnisharp\\Omnisharp.exe", "--languageserver" }
-  -- lsp_config["omnisharp"].cmd = { "C:/Users/janbo/.omnisharp/Omnisharp.exe", "--languageserver" }
-else
-  lsp_config["omnisharp"].cmd = { "/Users/jan/x/work/omnisharp-roslyn/mono-packaging/run", "--languageserver" }
-end
-
-
-
 -- local utils = require('nimet/utils')
 -- local is_windows = utils.isWindows()
--- lsp_config["omnisharp"].setup{
---   on_attach = on_attach,
 
 -- if is_windows then
---   cmd = { "C:/Users/janbo/.omnisharp/Omnisharp.exe", "--languageserver"},
+--   lsp_config["omnisharp"].cmd = { "C:\\Users\\janbo\\.omnisharp\\Omnisharp.exe", "--languageserver" }
 -- else
---   cmd = { "/Users/jan/x/work/omnisharp-roslyn/mono-packaging/run", "--languageserver"},
+--   lsp_config["omnisharp"].cmd = { "/Users/jan/x/work/omnisharp-roslyn/mono-packaging/run", "--languageserver" }
 -- end
+
+-- lsp_config["omnisharp"].setup{
+--   on_attach = on_attach
 -- }
 
+-- -- lsp_config.omnisharp.setup({
+-- --   on_attach = on_attach
+-- -- })
 
-lsp_zero.setup()
+-- Now setup your LSP servers with lsp-zero
+lsp.setup()
 
+-- Diagnostic configuration
 vim.diagnostic.config({
     virtual_text = true
 })
-
--- require'lspconfig'.omnisharp.setup{
---   -- other configurations
---   autostart = false,
--- }
 
