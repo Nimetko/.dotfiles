4cf550d3dcac59d77fc3c6e6fe36eb51d0f85643
Nimetko <janborovsky01@gmail.com>
2024-08-12 23:53:19 +0200

    remove harpoon

--- nvim/lua/nimet/lazy/frompacker.lua
+++ nvim/lua/nimet/lazy/frompacker.lua
@@ -11,7 +11,6 @@ return {
 	'preservim/nerdtree',
 	"tpope/vim-fugitive",
 	'thosakwe/vim-flutter',
-	-- 'theprimeagen/harpoon',
     {'akinsho/toggleterm.nvim', version = "*", config = true},
 	'mbbill/undotree',
 	'tpope/vim-commentary',
