1f2efcf9e6e13a67310aa6e8a561a7ac52aad2d1
Nimetko <janborovsky01@gmail.com>
2024-09-07 06:34:54 +0200

    use on Attach for lua and other

--- nvim/.config/nvim/lua/nimet/lazy/lsp.lua
+++ nvim/.config/nvim/lua/nimet/lazy/lsp.lua
@@ -9,6 +9,7 @@ return {
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
+        "nvim-telescope/telescope-ui-select.nvim"
     },
 
     config = function()
@@ -20,6 +21,30 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities()
         )
+
+        -- Define your custom on_attach function to set LSP key mappings
+        local function myFunction(opts)
+            vim.keymap.set("n", "<leader>lsp", function() print("LSP") end)
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references({ use_quickfix = true }) end)
+            vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.formatting() end, opts)
+        end
+
+        -- On attach function for LSP key mappings
+        local on_attach = function(client, bufnr)
+            local opts = { noremap=true, silent=true, buffer=bufnr }
+            myFunction(opts)
+        end
+
+
 
         require("mason").setup()
         require("mason-lspconfig").setup({
@@ -52,28 +77,6 @@ return {
                 end,
             }
         })
-
-        -- Define your custom on_attach function to set LSP key mappings
-        local function myFunction(opts)
-            vim.keymap.set("n", "<leader>lsp", function() print("LSP") end)
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references({ use_quickfix = true }) end)
-            vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.formatting() end, opts)
-        end
-
-        -- On attach function for LSP key mappings
-        local on_attach = function(client, bufnr)
-            local opts = { noremap=true, silent=true, buffer=bufnr }
-            myFunction(opts)
-        end
 
         -- Example of configuring dartls with a specific on_attach function
         local lsp_config = require("lspconfig")
