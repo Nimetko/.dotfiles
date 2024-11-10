9aeb41353d346d33d15b06867b4e494b7d47b029
Nimetko <janborovsky01@gmail.com>
2024-11-10 10:30:01 +0100

    dart comments for tpope commentary

--- nvim/.config/nvim/lua/nimet/init.lua
+++ nvim/.config/nvim/lua/nimet/init.lua
@@ -16,3 +16,12 @@ require('toggleterm').setup{
 -- vim.o.number = true  -- Show line numbers
 
 require('nimet/customCommands')
+
+-- dart comments for commentary tpope plugin
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dart",
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
+
