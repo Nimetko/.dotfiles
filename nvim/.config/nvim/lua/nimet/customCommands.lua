f805c6b0cc50040b9d0d24ab04c4338a14400e7e
Jan Borovsky <jan.borovsky@consultant.lego.com>
2025-03-26 19:56:34 +0100

    Copy File Name custom command

--- nvim/.config/nvim/lua/nimet/customCommands.lua
+++ nvim/.config/nvim/lua/nimet/customCommands.lua
@@ -3,3 +3,7 @@ vim.api.nvim_create_user_command('CopyPath', function()
   vim.fn.setreg('+', vim.fn.expand('%:p'))
 end, {})
 
+vim.api.nvim_create_user_command('CopyFileName', function()
+  vim.fn.setreg('+', vim.fn.expand('%:t'))
+end, {})
+
